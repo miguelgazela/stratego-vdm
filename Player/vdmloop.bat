7e33993787cfe51533731d856c082406597524b0
miguelgazela <miguel.gazela@gmail.com>
2013-12-01 15:04:20 +0000

    Player tests working and rtf files added

--- Player/vdmloop.bat
+++ Player/vdmloop.bat
@@ -2,9 +2,9 @@
 rem Runs a collection of VDM++ test examples
 rem Assumes specification is in Word RTF files
 
+set S0=../Piece/Piece.rtf
 set S1=Player.rtf
 set S2=TestPlayer.rtf
 
-
-"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -p -R vdm.tc %S1% %S2%
+"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -p -R vdm.tc %S0% %S1% %S2%
 for /R %%f in (*.arg) do call vdmtest "%%f"
--- Player/vdmtest.bat
+++ Player/vdmtest.bat
@@ -5,11 +5,12 @@ echo VDM Test: '%1' > con
 echo VDM Test: '%1'
 
 rem short names for specification files in Word RTF Format
+set S0=../Piece/Piece.rtf
 set S1=Player.rtf
 set S2=TestPlayer.rtf
 
 rem -- Calls the interpreter for this test case
-"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S1% %S2%
+"C:\Program Files\The VDM++ Toolbox v9.0.2\bin\vppde" -i -D -I -P -Q -R vdm.tc -O %1.res %1 %S0% %S1% %S2%
 
 rem -- Check for difference between result of execution and expected result.
 if EXIST %1.exp fc /w %1.res %1.exp
